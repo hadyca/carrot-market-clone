c734b6c465b0148af5089662758d9bb9b203451b
Jae Hyung Kim <SC425@ypsa.com>
2024-08-01 15:39:01 +0600

    모달창

---
+++ app/products/add/schema.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const productSchema = z.object({
+  photo: z.string({
+    required_error: "Photo is required",
+  }),
+  title: z
+    .string({
+      required_error: "Title is required!!!!!",
+    })
+    .min(5, "5이상해라"),
+  description: z.string({
+    required_error: "Description is required",
+  }),
+  price: z.coerce.number({
+    required_error: "Price is required",
+  }),
+});
+
+export type ProductType = z.infer<typeof productSchema>;
